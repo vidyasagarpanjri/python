a54d6165aaafb5204356b914adbe4e6206af04a0
Vidya Sagar Panjri <vidyasagar.panjri@gmail.com>
2017-03-31 17:24:29 +0530

    Create PE_headers.py

---
+++ PE_headers.py
@@ -0,0 +1,17 @@
+import pefile
+import glob
+import datetime
+
+
+#print d
+def FaritPE(folder):
+    ret_list=[]
+    d = glob.glob(folder)
+    for f in d:
+        p = pefile.PE(f)
+        ret_list.append([datetime.datetime.fromtimestamp(p.FILE_HEADER.TimeDateStamp),p.DOS_HEADER.e_lfanew])
+    return ret_list
+    
+
+if __name__=='__main__':
+    print FaritPE("C:\\path\\to\\exe\\folder\\*")
